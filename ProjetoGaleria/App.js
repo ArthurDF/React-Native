65c719b8ccb403b114e21d7affafa3f45649f69a
Arthur Diniz Flor Torquato Fernandes <arthurdinizflor@gmail.com>
2019-09-02 20:38:43 -0300

    Estrutura Primária da Galeria Criada
    Nessa Etapa Foram Adicionadas Funções Que Acessam Um Arquivo JSON e Acessam As Informações Contidas Nele E A Partir Delas Montam Uma Lista Com As Imagens Contidas Neles

--- ProjetoGaleria/App.js
+++ ProjetoGaleria/App.js
@@ -1,22 +1,100 @@
-import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Platform,
+  View,
+  ActivityIndicator,
+  FlatList,
+  Text,
+  Image,
+  Alert,
+  YellowBox
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={[{ key: "a" }, { key: "b" }]}
-        renderItem={({ item }) => <Text>{item.key}</Text>}
-      />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true
+    };
+  }
+  webCall = () => {
+    return fetch("https://reactnativecode.000webhostapp.com/FlowersList.php")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: responseJson
+          },
+          function() {
+            // In this block you can do something with new state.
+          }
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  componentDidMount() {
+    this.webCall();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          ItemSeparatorComponent={this.FlatListItemSeparator}
+          renderItem={({ item }) => (
+            <View style={{ flex: 1, flexDirection: "row" }}>
+              <Image
+                source={{ uri: item.flower_image_url }}
+                style={styles.imageView}
+              />
+            </View>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  MainContainer: {
+    justifyContent: "center",
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    margin: 5,
+    marginTop: Platform.OS === "ios" ? 20 : 0
+  },
+  container: {
+    flex: 1
+  },
+
+  imageView: {
+    width: "50%",
+    height: 100,
+    margin: 7,
+    borderRadius: 7
+  },
+
+  textView: {
+    width: "50%",
+    textAlignVertical: "center",
+    padding: 10,
+    color: "#000"
   }
 });
