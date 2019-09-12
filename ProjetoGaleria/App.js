24cf867ae258f621a6d87f7c8cf722d8bae5adec
Arthur Diniz Flor Torquato Fernandes <arthurdinizflor@gmail.com>
2019-09-12 19:48:53 -0300

    FullScreen
    Adicionada Função de FullScreen ao App da galeria

--- ProjetoGaleria/App.js
+++ ProjetoGaleria/App.js
@@ -7,16 +7,18 @@ import {
   FlatList,
   Text,
   Image,
-  Alert,
-  YellowBox
+  TouchableOpacity,
+  Modal
 } from "react-native";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-
+    this._onPress = this._onPress.bind(this);
     this.state = {
-      isLoading: true
+      isLoading: true,
+      ModalVisible: false,
+      TempImageURL: "https://i.pinimg.com/originals/af/41/30/af4130615f3fe2228cce052130ab0280.jpg"
     };
   }
   webCall = () => {
@@ -26,11 +28,11 @@ export default class App extends Component {
         this.setState(
           {
             isLoading: false,
-            dataSource: responseJson
-          },
-          function() {
-            // In this block you can do something with new state.
+            dataSource: responseJson,
           }
+          /*function() {
+            // In this block you can do something with new state.
+          }*/
         );
       })
       .catch(error => {
@@ -41,7 +43,14 @@ export default class App extends Component {
   componentDidMount() {
     this.webCall();
   }
-
+  _onPress(moda,imgUrl){
+    
+    this.setState({
+      ModalVisible: moda,
+      TempImageURL: imgUrl
+    });
+    //console.log(this.state.TempImageURL);
+  }
   render() {
     if (this.state.isLoading) {
       return (
@@ -55,6 +64,16 @@ export default class App extends Component {
 
     return (
       <View style={styles.container}>
+        <Modal
+          transparent={false}
+          visible={this.state.ModalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+          }}
+        >
+          <Image source={{ uri: this.state.TempImageURL }} style={{flex:1}} />
+        </Modal>
+
         <View>
           <Text style={styles.textView}>ESPAÇO PARA OS MENUS SUPERIORES</Text>
         </View>
@@ -62,11 +81,15 @@ export default class App extends Component {
           data={this.state.dataSource}
           ItemSeparatorComponent={this.FlatListItemSeparator}
           renderItem={({ item }) => (
-            <View style={{ flex: 1,marginRight:3}}>
-              <Image
-                source={{ uri: item.flower_image_url }}
-                style={styles.imageView}
-              />
+            <View style={{ flex: 1, marginRight: 3 }}>
+              <TouchableOpacity
+                onPress={() => this._onPress(true,item.flower_image_url)}
+              >
+                <Image
+                  source={{ uri: item.flower_image_url }}
+                  style={styles.imageView}
+                />
+              </TouchableOpacity>
             </View>
           )}
           keyExtractor={(item, index) => index.toString()}
@@ -85,30 +108,31 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flex: 1,
     margin: 5,
-    marginTop: Platform.OS === "ios" ? 20 : 0,
-    
+    marginTop: Platform.OS === "ios" ? 20 : 0
   },
   container: {
-    justifyContent: 'center',
+    justifyContent: "center",
     flex: 1,
-    paddingTop: 30,
+    paddingTop: 30
   },
 
   imageView: {
     /*width: "50%",
     height: 100,*/
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 100,
     width: "100%",
-    margin:3,
-    
+    margin: 3
+  },
+  ima: {
+    flex: 1
   },
 
   textView: {
     width: "100%",
     textAlign: "center",
     color: "#000",
-    marginBottom:2
+    marginBottom: 2
   }
 });
